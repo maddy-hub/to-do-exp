97e2926a76a1480adc5cd499bce912589dbca1f3
Maddy <maddy@madhu.m>
2020-08-18 22:57:36 +0530

    * Crated todo app by reading the todo data from the todoData file

---
+++ src/todoData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+      id: 1,
+      text: "learn react",
+      completed: true
+  },
+  {
+      id: 2,
+      text: "understand redux",
+      completed: false
+  },
+  {
+      id: 3,
+      text: "clean up the code",
+      completed: false
+  },
+  {
+      id: 4,
+      text: "move the app to github",
+      completed: true
+  },
+  {
+      id: 5,
+      text: "make API call in react using axios",
+      completed: true
+  }
+]
+
+export default todosData
